579fe2d225933d3c71bcaed5db7490fa3de59506
Mauro Michielon <mauro.michielon@eea.europa.eu>
2019-10-14 11:47:09 +0200

    Update inotify_apache_restart.sh

--- inotify_apache_restart.sh
+++ inotify_apache_restart.sh
@@ -9,6 +9,5 @@ inotifywait -m --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
 -e close_write $FILE | while read date time dir file; do
        sleep 1
        echo apachectl -k graceful
-       #rsync --progress --relative -vrae 'ssh -p 22'  $FILECHANGEREL usernam@example.com:/backup/root/dir && \
        echo "At ${time} on ${date}, apachectl -k graceful"
 done
